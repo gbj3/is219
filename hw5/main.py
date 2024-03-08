fc103a9b19637f52e64ddc5574c35cfe660c0903
gbj3 <gbj3@njit.edu>
2024-03-07 22:40:51 -0500

    Fixed init env and logging

--- hw5/main.py
+++ hw5/main.py
@@ -1,5 +1,5 @@
 # main.py
-from app.__init__ import App    
+from app.__init__ import App
 
 # You must put this in your main.py because this forces the program to start when you run it from the command line.
 if __name__ == "__main__":
